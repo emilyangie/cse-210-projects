72450631dcbd53d6285141cfe0f832c9d6f47c5c
emilyangie <angie.mudzimba@gmail.com>
2023-11-13 13:08:35 +0200

    learning 02 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -3,7 +3,25 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+{
+        Job job1 = new Job();
+        job1._jobTitle = "Sales Representative";
+        job1._company = "Jan Jam Wear";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Airbnb";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Emily Mudzimba";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
